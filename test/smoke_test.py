301816961a4f0c7a5e8ef9dd654860699a9a67d3
Tom Begley <tomcbegley@gmail.com>
2022-10-21 10:30:30 +0100

    Add smoke test

---
+++ test/smoke_test.py
@@ -0,0 +1,4 @@
+def test_imports():
+    from tensordict import TensorDict  # noqa: F401
+    from tensordict.nn import TensorDictModule  # noqa: F401
+    from tensordict.tensor_specs import TensorSpec  # noqa: F401
