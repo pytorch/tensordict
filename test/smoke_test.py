72e3ec48718704c756ac4a132759eceaba3cc92b
Tom Begley <tomcbegley@gmail.com>
2022-10-24 13:58:37 +0100

    Update tests

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,4 +1,3 @@
 def test_imports():
     from tensordict import TensorDict  # noqa: F401
     from tensordict.nn import TensorDictModule  # noqa: F401
-    from tensordict.tensor_specs import TensorSpec  # noqa: F401
