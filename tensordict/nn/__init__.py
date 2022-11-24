ac7c9930565cbac93d6df4a2e935d79eea66105f
vmoens <vincentmoens@gmail.com>
2022-11-24 15:55:22 +0000

    init

--- tensordict/nn/__init__.py
+++ tensordict/nn/__init__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .common import *
-from .sequence import *
-from .probabilistic import *
+from .common import TensorDictModule, TensorDictModuleWrapper
+from .probabilistic import ProbabilisticTensorDictModule
+from .sequence import TensorDictSequential
