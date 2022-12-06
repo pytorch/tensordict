6af4d5ebf2805090103d20017a1a6be70bc37082
Naji El Hachem <najielhachem@meta.com>
2022-12-06 11:54:10 +0100

    use pybind tuple instead of std tuple

--- tensordict/cpp/tensormap.h
+++ tensordict/cpp/tensormap.h
@@ -9,9 +9,9 @@ class TensorMap {
         TensorMap();
         void set(std::string key, torch::Tensor value);
         void set(std::string key, TensorMap value);
-        void set(std::tuple key, torch::Tensor value);
-        void set(std::tuple key, TensorMap value);
+        void set(pybind11::tuple key, torch::Tensor value);
+        void set(pybind11::tuple key, TensorMap value);
         std::variant<torch::Tensor, TensorMap> get(std::string key);
-        std::variant<torch::Tensor, TensorMap> get(std::tuple key);
+        std::variant<torch::Tensor, TensorMap> get(pybind11::tuple key);
         // TODO add keys - check iterator
-}
+};
