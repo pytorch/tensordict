2442208370efbb9fa6a6847a9e50c5e7952dbfc7
Naji El Hachem <najielhachem@meta.com>
2022-12-07 18:46:35 +0100

    Pass all arguments by reference

--- tensordict/cpp/tensormap.h
+++ tensordict/cpp/tensormap.h
@@ -9,11 +9,12 @@ class TensorMap {
             int index);
         // TODO something about batch size
     public:
-        void SetTensorAt(std::string key, torch::Tensor value);
-        void SetMapAt(std::string key, TensorMap value);
-        void SetTensorAtPath(std::vector<std::string> key, torch::Tensor value);
-        void SetMapAtPath(std::vector<std::string> key, TensorMap value);
-        std::variant<torch::Tensor, TensorMap> GetAt(std::string key);
-        std::variant<torch::Tensor, TensorMap> GetAtPath(std::vector<std::string> key);
+        // TODO Add const to all input args
+        void SetTensorAt(std::string key, torch::Tensor& value);
+        void SetMapAt(std::string key, TensorMap& value);
+        void SetTensorAtPath(std::vector<std::string>& key, torch::Tensor& value);
+        void SetMapAtPath(std::vector<std::string>& key, TensorMap& value);
+        std::variant<torch::Tensor, TensorMap>& GetAt(std::string key);
+        std::variant<torch::Tensor, TensorMap>& GetAtPath(std::vector<std::string>& key);
         // TODO add keys - check iterator
 };
