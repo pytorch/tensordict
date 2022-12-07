4a68032fc0b4db3c6e333bc0f86ea8d6a46bf1e6
Naji El Hachem <najielhachem@meta.com>
2022-12-07 19:43:59 +0100

    Store pointers of tensor and class in internal map

--- tensordict/cpp/tensormap.cpp
+++ tensordict/cpp/tensormap.cpp
@@ -4,12 +4,13 @@
 
 void TensorMap::SetTensorAt(std::string key, torch::Tensor& value)
 {
-    this->map[key] = value;
+    // we want to pass objects as arguments but store the reference
+    this->map[key] = &value;
 }
 
 void TensorMap::SetMapAt(std::string key, TensorMap& value)
 {
-    this->map[key] = value;
+    this->map[key] = &value;
 }
 
 void TensorMap::SetTensorAtPath(std::vector<std::string>& indices, torch::Tensor& value)
@@ -20,7 +21,7 @@ void TensorMap::SetTensorAtPath(std::vector<std::string>& indices, torch::Tensor
     auto lastMap = GetRecursive(this->map, indices, 0);
     auto key = indices[indices.size() - 1];
 
-    lastMap->at(key) = value;
+    lastMap[key] = &value;
 }
 
 void TensorMap::SetMapAtPath(std::vector<std::string>& indices, TensorMap& value)
@@ -31,18 +32,18 @@ void TensorMap::SetMapAtPath(std::vector<std::string>& indices, TensorMap& value
     auto lastMap = GetRecursive(this->map, indices, 0);
     auto key = indices[indices.size() - 1];
 
-    lastMap->at(key) = value;
+    lastMap[key] = &value;
 }
 
-std::variant<torch::Tensor, TensorMap>& TensorMap::GetAt(std::string key)
+std::variant<torch::Tensor, TensorMap> TensorMap::GetAt(std::string key)
 {
     if (this->map.count(key) == 0)
         throw std::invalid_argument("Invalid key: " + key);
 
-    return this->map[key];
+    return UnboxVariant(this->map[key]);
 }
 
-std::variant<torch::Tensor, TensorMap>& TensorMap::GetAtPath(std::vector<std::string>& indices)
+std::variant<torch::Tensor, TensorMap> TensorMap::GetAtPath(std::vector<std::string>& indices)
 {
     if (indices.size() == 0)
         throw std::invalid_argument("indices must have at least one element");
@@ -50,11 +51,13 @@ std::variant<torch::Tensor, TensorMap>& TensorMap::GetAtPath(std::vector<std::st
     auto lastMap = GetRecursive(this->map, indices, 0);
     auto key = indices[indices.size() - 1];
 
-    return lastMap->at(key);
+    return UnboxVariant(lastMap[key]);
 }
 
-std::map<std::string, std::variant<torch::Tensor, TensorMap>>* TensorMap::GetRecursive(
-    std::map<std::string, std::variant<torch::Tensor, TensorMap>>& map,
+// Helper methods
+
+std::map<std::string, std::variant<torch::Tensor*, TensorMap*>>& TensorMap::GetRecursive(
+    std::map<std::string, std::variant<torch::Tensor*, TensorMap*>>& map,
     std::vector<std::string>& indices,
     int index)
 {
@@ -64,14 +67,26 @@ std::map<std::string, std::variant<torch::Tensor, TensorMap>>* TensorMap::GetRec
 
     if (index == indices.size() - 1)
     {
-        return &map;
+        return map;
     }
 
-    if (std::holds_alternative<TensorMap>(map[key]))
+    if (std::holds_alternative<TensorMap*>(map[key]))
     {
-        auto currentMap = std::get<TensorMap>(map[key]);
-        return GetRecursive(currentMap.map, indices, index + 1);
+        auto currentMap = std::get<TensorMap*>(map[key]);
+        return GetRecursive(currentMap->map, indices, index + 1);
     }
     else
         throw std::invalid_argument("Expected to have a Map at index " + std::to_string(index) + " but found tensor");
+}
+
+std::variant<torch::Tensor, TensorMap> TensorMap::UnboxVariant(std::variant<torch::Tensor*, TensorMap*> pointer)
+{
+    if (std::holds_alternative<TensorMap*>(pointer))
+    {
+        TensorMap* value = std::get<TensorMap*>(pointer);
+        return std::variant<torch::Tensor, TensorMap>(*value);
+    }
+
+    torch::Tensor* value = std::get<torch::Tensor*>(pointer);
+    return std::variant<torch::Tensor, TensorMap>(*value);
 }
--- tensordict/cpp/tensormap.h
+++ tensordict/cpp/tensormap.h
@@ -2,11 +2,9 @@
 
 class TensorMap {
     private:
-        std::map<std::string, std::variant<torch::Tensor, TensorMap>> map;
-        std::map<std::string, std::variant<torch::Tensor, TensorMap>>* GetRecursive(
-            std::map<std::string, std::variant<torch::Tensor, TensorMap>>& map,
-            std::vector<std::string>& indices,
-            int index);
+        std::map<std::string, std::variant<torch::Tensor*, TensorMap*>> map; // store references instead of copies
+        std::variant<torch::Tensor, TensorMap> UnboxVariant(std::variant<torch::Tensor*, TensorMap*> pointer);
+        std::map<std::string, std::variant<torch::Tensor*, TensorMap*>>& GetRecursive(std::map<std::string, std::variant<torch::Tensor*, TensorMap*>>& map, std::vector<std::string>& indices, int index);
         // TODO something about batch size
     public:
         // TODO Add const to all input args
@@ -14,7 +12,7 @@ class TensorMap {
         void SetMapAt(std::string key, TensorMap& value);
         void SetTensorAtPath(std::vector<std::string>& key, torch::Tensor& value);
         void SetMapAtPath(std::vector<std::string>& key, TensorMap& value);
-        std::variant<torch::Tensor, TensorMap>& GetAt(std::string key);
-        std::variant<torch::Tensor, TensorMap>& GetAtPath(std::vector<std::string>& key);
+        std::variant<torch::Tensor, TensorMap> GetAt(std::string key);
+        std::variant<torch::Tensor, TensorMap> GetAtPath(std::vector<std::string>& key);
         // TODO add keys - check iterator
 };
