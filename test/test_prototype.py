7e0e8e299eff714f7fceff5750a503adc176c61c
Naji El Hachem <najielhachem@meta.com>
2022-12-19 15:33:05 +0100

    [Test] Add test_simple_set_tensor

--- test/test_prototype.py
+++ test/test_prototype.py
@@ -1,30 +1,19 @@
 import torch
-from tensor_map_cpp import TensorMap
 import pytest
+
 from _utils_internal import get_available_devices
+from tensordict import TensorMap
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-def test_tensordict_set(device):
-    torch.manual_seed(1)
-    td = TensorMap()
-    val = torch.randn(4, 5)
-    td.set("key1", val)
-    assert (td.get("key1") == val).all()
-
-    td.set(["key2", "key3"], torch.ones(4, 5))
-    assert (td.get("key2").get("key3") == 1).all()
-
-
-def test_tensordict_ref():
+def test_tensormap_simple_set_tensor(device):
     m = TensorMap()
-    x = torch.randn(3)
-    m.set('a', x)
-    assert m.get('a') is x
-    n = TensorMap()
-    m.set('b', n)
-    y = torch.randn(3)
-    n.set('c', y)
-    assert m.get('b') is n
-    assert m.get('b').get('c') is m.get(['b', 'c'])
-    assert m.get(['b', 'c']) is y
+    x = torch.ones(3)
+    m['a'] = x
+    assert m['a'] is x
+
+    m['a'] = torch.zeros(3)
+    assert (m['a'] == 0).all()
+
+    m['b'] = m['a']
+    assert m['b'] is m['a']
