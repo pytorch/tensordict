1e8a2a478edd4e54f0b878a382cece7dffce8fae
Tom Begley <tomcbegley@gmail.com>
2022-12-19 17:17:07 +0000

    Add device test to no_future

--- test/test_tensorclass.py
+++ test/test_tensorclass.py
@@ -25,19 +25,6 @@ class MyData:
         return self.X + self.y
 
 
-@pytest.mark.parametrize("device", get_available_devices())
-def test_device(device):
-    data = MyData(
-        X=torch.ones(3, 4, 5),
-        y=torch.zeros(3, 4, 5, dtype=torch.bool),
-        batch_size=[3, 4],
-        device=device,
-    )
-    assert data.device == device
-    assert data.X.device == device
-    assert data.y.device == device
-
-
 def test_dataclass():
     data = MyData(
         X=torch.ones(3, 4, 5),
@@ -54,6 +41,19 @@ def test_type():
         batch_size=[3, 4],
     )
     assert isinstance(data, MyData)
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_device(device):
+    data = MyData(
+        X=torch.ones(3, 4, 5),
+        y=torch.zeros(3, 4, 5, dtype=torch.bool),
+        batch_size=[3, 4],
+        device=device,
+    )
+    assert data.device == device
+    assert data.X.device == device
+    assert data.y.device == device
 
 
 def test_banned_types():
