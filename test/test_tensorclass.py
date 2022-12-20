7f4b456378c6dbfb50e2d04740bc1f3a89584381
Tom Begley <tomcbegley@gmail.com>
2022-12-20 09:27:48 +0000

    delete duplicated test

--- test/test_tensorclass.py
+++ test/test_tensorclass.py
@@ -69,19 +69,6 @@ def test_device(device):
         data.device = torch.device("cpu")
 
 
-@pytest.mark.parametrize("device", get_available_devices())
-def test_device(device):
-    data = MyData(
-        X=torch.ones(3, 4, 5),
-        y=torch.zeros(3, 4, 5, dtype=torch.bool),
-        batch_size=[3, 4],
-        device=device,
-    )
-    assert data.device == device
-    assert data.X.device == device
-    assert data.y.device == device
-
-
 def test_banned_types():
     @tensorclass
     class MyAnyClass:
