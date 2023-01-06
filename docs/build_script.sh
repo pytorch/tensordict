38e9bcb853edcaa95c273a8b81b6eff25dc96da0
Vincent Moens <vincentmoens@gmail.com>
2023-01-06 18:32:31 +0000

    [Doc] More tutorials: ImageNet (#152)
    Co-authored-by: Tom Begley <tomcbegley@gmail.com>

--- docs/build_script.sh
+++ docs/build_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bash
 
 rm -rf _local_build build generated
-#sphinx-autogen -o generated source/reference/*.rst && sphinx-build ./source _local_build && 
-make html
+#sphinx-autogen -o generated source/reference/*.rst && sphinx-build ./source _local_build &&
+make docs
