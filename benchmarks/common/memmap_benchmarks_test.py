79d8607a69eea1efb30824409b3a5b2ef640464c
Tom Begley <tomcbegley@gmail.com>
2023-01-30 12:43:53 +0000

    [Benchmarks] Add MemmapTensor benchmarks (#197)

---
+++ benchmarks/common/memmap_benchmarks_test.py
@@ -0,0 +1,46 @@
+import pytest
+import torch
+
+from tensordict import MemmapTensor
+
+
+def get_available_devices():
+    devices = [torch.device("cpu")]
+    n_cuda = torch.cuda.device_count()
+    if n_cuda > 0:
+        for i in range(n_cuda):
+            devices += [torch.device(f"cuda:{i}")]
+    return devices
+
+
+@pytest.fixture
+def tensor():
+    return torch.zeros(3, 4, 5)
+
+
+@pytest.fixture(params=get_available_devices())
+def memmap_tensor(request):
+    return MemmapTensor(3, 4, 5, device=request.param)
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+def test_creation(benchmark, device):
+    benchmark.pedantic(
+        MemmapTensor, args=(3, 4, 5), kwargs={"device": device}, iterations=10
+    )
+
+
+def test_creation_from_tensor(benchmark, tensor):
+    benchmark.pedantic(MemmapTensor.from_tensor, args=(tensor,), iterations=10)
+
+
+def test_add_one(benchmark, memmap_tensor):
+    benchmark.pedantic(lambda: memmap_tensor + 1, iterations=10_000)
+
+
+def test_contiguous(benchmark, memmap_tensor):
+    benchmark.pedantic(lambda: memmap_tensor.contiguous(), iterations=10_000)
+
+
+def test_stack(benchmark, memmap_tensor):
+    benchmark.pedantic(torch.stack, args=([memmap_tensor] * 2, 0), iterations=10)
