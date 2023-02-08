8b80ac405d27261adfd68707d1e660adceb445e3
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2023-02-08 10:21:41 +0100

    Update test_tensorclass_nofuture.py

--- test/test_tensorclass_nofuture.py
+++ test/test_tensorclass_nofuture.py
@@ -353,21 +353,19 @@ def test_setitem():
         match="Meta data at 'z' may or may not be equal, this may result in undefined behaviours",
     ):
         data[:2] = data3[:2]
-    
+
     # regression test PR #203
     # We should be able to set tensors items with MemmapTensors and viceversa
     @tensorclass
     class MyDataMemMap1:
         x: torch.Tensor
         y: MemmapTensor
-    
-    
+
     @tensorclass
     class MyDataMemMap2:
         x: MemmapTensor
         y: torch.Tensor
-    
-    
+
     data1 = MyDataMemMap1(
         x=torch.zeros(3, 4, 5),
         y=MemmapTensor.from_tensor(torch.zeros(3, 4, 5), transfer_ownership=True),
