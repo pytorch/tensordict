13f39041e5d43f2021daab0f8e9f7aca57b4ab70
Tom Begley <tomcbegley@gmail.com>
2023-02-09 09:51:33 +0000

    Address comments

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+
 import numpy as np
 import pytest
 import torch
@@ -82,3 +89,8 @@ def test_getitem_batch_size_mask(tensor, idx, ndim, slice_leading_dims):
     else:
         index = (0,) * idx + (mask,)
     assert tensor[index].shape == _getitem_batch_size(tensor.shape, index)
+
+
+if __name__ == "__main__":
+    args, unknown = argparse.ArgumentParser().parse_known_args()
+    pytest.main([__file__, "--capture", "no", "--exitfirst"] + unknown)
