10f2a82be99110e7a1da0265442fed70b09a0afb
Tom Begley <tomcbegley@gmail.com>
2023-02-09 13:38:41 +0000

    Fix recursive setitem with index

--- tensordict/tensordict.py
+++ tensordict/tensordict.py
@@ -2081,14 +2081,30 @@ class TensorDictBase(Mapping, metaclass=abc.ABCMeta):
                     f"(batch_size = {self.batch_size}, index={index}), "
                     f"which differs from the source batch size {value.batch_size}"
                 )
-            keys = set(self.keys())
-            if not all(key in keys for key in value.keys()):
-                subtd = self.get_sub_tensordict(index)
-            for key, item in value.items():
-                if key in keys:
+            subtd = None
+            autonested_keys = []
+            for key in _TensorDictKeysView(
+                value,
+                include_nested=True,
+                leaves_only=True,
+                error_on_loop=False,
+                yield_autonested_keys=True,
+            ):
+                if isinstance(key, _NestedKey):
+                    autonested_keys.append(key)
+                    continue
+                item = value.get(key)
+                if key in self.keys(include_nested=True):
                     self.set_at_(key, item, index)
                 else:
+                    if subtd is None:
+                        subtd = self.get_sub_tensordict(index)
                     subtd.set(key, item)
+
+            for root_key, nested_key in autonested_keys:
+                self.set(
+                    nested_key, self.get(root_key) if root_key is not None else self
+                )
 
     def __delitem__(self, index: INDEX_TYPING) -> TensorDictBase:
         # if isinstance(index, str):
@@ -4885,6 +4901,11 @@ class LazyStackedTensorDict(TensorDictBase):
         self._is_memmap = False
         for td in self.tensordicts:
             td.unlock()
+        return self
+
+    def zero_(self):
+        for td in self.tensordicts:
+            td.zero_()
         return self
 
 
