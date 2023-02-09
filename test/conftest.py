3f396a350e48be93c2395a070ec0b6b655a230ad
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2023-02-09 15:38:15 +0100

    amend

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import tempfile
 from pathlib import Path
-
-import pytest
 
 HERE = Path(__file__).parent
 
