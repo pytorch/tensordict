2d72d0793f3be9bbd827ee6df00885895fcea9df
Tom Begley <tomcbegley@gmail.com>
2023-02-23 16:10:54 +0000

    Address comments

--- tutorials/sphinx_tuto/tensordict_preallocation.py
+++ tutorials/sphinx_tuto/tensordict_preallocation.py
@@ -2,14 +2,14 @@
 """
 Pre-allocating memory with TensorDict
 =====================================
-**Author**: `Vincent Moens <https://github.com/vmoens>`_
+**Author**: `Tom Begley <https://github.com/tcbegley>`_
 
 In this tutorial you will learn how to take advantage of memory pre-allocation in
-``TensorDict``.
+:class:`~.TensorDict`.
 """
 
 ##############################################################################
-# Suppose that we have a function that returns a ``TensorDict``
+# Suppose that we have a function that returns a :class:`~.TensorDict`
 
 
 # sphinx_gallery_start_ignore
@@ -27,7 +27,7 @@ def make_tensordict():
 
 ###############################################################################
 # Perhaps we want to call this function multiple times and use the results to populate
-# a single ``TensorDict``.
+# a single :class:`~.TensorDict`.
 
 N = 10
 tensordict = TensorDict({}, batch_size=[N, 3])
@@ -44,14 +44,36 @@ print(tensordict)
 # value of ``make_tensordict``. In the above example, we pre-allocate an array of zeros
 # of size ``torch.Size([10, 3])`` for the key ``"a"``, and an array size
 # ``torch.Size([10, 3, 4])`` for the key ``"b"``. Subsequent iterations of the loop are
-# written in place.
+# written in place. As a result, if not all values are filled, they get the default
+# value of zero.
 #
-# As a result, if not all values are filled, they get the default value of zero.
+# Let us demonstrate what is going on by stepping through the above loop. We first
+# initialise an empty :class:`~.TensorDict`.
 
 N = 10
 tensordict = TensorDict({}, batch_size=[N, 3])
+print(tensordict)
 
-for i in range(2):
-    tensordict[i] = make_tensordict()
+##############################################################################
+# After the first iteration, ``tensordict`` has been prepopulated with tensors for both
+# ``"a"`` and ``"b"``. These tensors contain zeros except for the first row which we
+# have assigned random values to.
 
-assert (tensordict[2:] == 0).all()
+random_tensordict = make_tensordict()
+tensordict[0] = random_tensordict
+
+assert (tensordict[1:] == 0).all()
+assert (tensordict[0] == random_tensordict).all()
+
+print(tensordict)
+
+##############################################################################
+# Subsequent iterations, we update the pre-allocated tensors in-place.
+
+a = tensordict["a"]
+random_tensordict = make_tensordict()
+tensordict[1] = random_tensordict
+
+# the same tensor is stored under "a", but the values have been updated
+assert tensordict["a"] is a
+assert (tensordict[:2] != 0).all()
