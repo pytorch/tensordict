07a7337d8fdbb3273bb36089fa3754597ad7787d
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2023-03-27 17:33:57 +0200

    [Refactor] avoid adding _TENSORCLASS flag (#301)
    Co-authored-by: vmoens <vincentmoens@gmail.com>

--- tensordict/prototype/tensorclass.py
+++ tensordict/prototype/tensorclass.py
@@ -63,7 +63,11 @@ _TD_PASS_THROUGH = {
 def is_tensorclass(obj: type | Any) -> bool:
     """Returns True if obj is either a tensorclass or an instance of a tensorclass."""
     cls = obj if isinstance(obj, type) else type(obj)
-    return dataclasses.is_dataclass(cls) and hasattr(cls, "__TENSORCLASS")
+    return (
+        dataclasses.is_dataclass(cls)
+        and "to_tensordict" in cls.__dict__
+        and "_from_tensordict" in cls.__dict__
+    )
 
 
 def tensorclass(cls: T) -> T:
@@ -188,7 +192,6 @@ def tensorclass(cls: T) -> T:
     cls.to_tensordict = _to_tensordict
     cls.device = property(_device, _device_setter)
     cls.batch_size = property(_batch_size, _batch_size_setter)
-    cls.__TENSORCLASS = True
 
     cls.__doc__ = f"{cls.__name__}{inspect.signature(cls)}"
 
