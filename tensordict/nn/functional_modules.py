a9ba41d2bebf44973d49c94c65860a657033c0eb
vmoens <vincentmoens@gmail.com>
2023-03-29 12:02:04 +0100

    amend

--- tensordict/nn/functional_modules.py
+++ tensordict/nn/functional_modules.py
@@ -218,17 +218,23 @@ def _decorate_funs(
 ) -> None:
     if funs_to_decorate is None:
         funs_to_decorate = ["forward"]
+    _is_functional = model.__dict__.get("_functionalized", False)
+    if not _is_functional:
+        model.__dict__["_functionalized"] = True
+        model.__dict__["_decorated_funs"] = set()
 
     for fun_to_decorate in funs_to_decorate:
+        if fun_to_decorate in model.__dict__["_decorated_funs"]:
+            continue
         try:
             setattr(
                 model,
                 fun_to_decorate,
                 types.MethodType(_make_decorator(model, fun_to_decorate), model),
             )
+            model.__dict__["_decorated_funs"].add(fun_to_decorate)
         except AttributeError:
             continue
-    model.__dict__["_functionalized"] = True
     if not model.__dict__.get("_is_stateless", False):
         model.__dict__["_is_stateless"] = make_stateless
 
