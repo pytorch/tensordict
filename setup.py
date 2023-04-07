9e32b5f1b3fce3ec44a65b44562c16e586892551
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2023-04-07 18:04:17 +0200

    lint

--- setup.py
+++ setup.py
@@ -128,7 +128,13 @@ def _main(argv):
         },
         install_requires=[pytorch_package_dep, "numpy", "cloudpickle"],
         extras_require={
-            "tests": ["pytest", "pyyaml", "pytest-instafail", "pytest-rerunfailures", "pytest-benchmark"],
+            "tests": [
+                "pytest",
+                "pyyaml",
+                "pytest-instafail",
+                "pytest-rerunfailures",
+                "pytest-benchmark",
+            ],
             "checkpointing": ["torchsnapshot-nightly"],
             "h5": ["h5py>=3.8"],
         },
