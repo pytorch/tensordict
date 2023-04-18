892c02b1792a48041fa6e9b01eb8050986c5b8d5
vmoens <vincentmoens@gmail.com>
2023-04-18 14:36:34 +0100

    amend

--- tensordict/_contextlib.py
+++ tensordict/_contextlib.py
@@ -91,7 +91,6 @@ def context_decorator(ctx, func):
     be a multi-shot context manager that can be directly invoked multiple times)
     or a callable that produces a context manager.
     """
-
     assert not (callable(ctx) and hasattr(ctx, "__enter__")), (
         f"Passed in {ctx} is both callable and also a valid context manager "
         "(has __enter__), making it ambiguous which interface to use.  If you "
