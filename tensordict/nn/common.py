5c1abedef0469b63874c9013c8a66e011b39b29c
vmoens <vincentmoens@gmail.com>
2023-05-25 15:09:06 +0100

    fix

--- tensordict/nn/common.py
+++ tensordict/nn/common.py
@@ -270,7 +270,6 @@ class dispatch:
                 out = func(_self, tensordict, *args, **kwargs)
                 out = tuple(out[key] for key in dest)
                 return out[0] if len(out) == 1 else out
-            print("calling with tensordict", id(tensordict))
             return func(_self, tensordict, *args, **kwargs)
 
         return wrapper
