4e19ef587ac50b41a72b031dfe599301743d08a1
vmoens <vincentmoens@gmail.com>
2023-06-15 21:24:32 +0100

    init

--- benchmarks/common/common_ops_test.py
+++ benchmarks/common/common_ops_test.py
@@ -24,6 +24,115 @@ def td(a, b):
     return TensorDict({"a": a, "b": {"b1": b}}, [3, 4])
 
 
+def big_td():
+    return (
+        (TensorDict({str(i): torch.zeros(3, 4) + i for i in range(100)}, [3, 4]),),
+        {},
+    )
+
+
+def big_nested_td():
+    return (
+        (
+            TensorDict(
+                {
+                    ".".join([str(j) for j in range(i)] + ["t"]): torch.zeros(3, 4) + i
+                    for i in range(1, 20)
+                },
+                [3, 4],
+            ).unflatten_keys("."),
+        ),
+        {},
+    )
+
+
+def test_items(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.items()), setup=big_td, rounds=1000, iterations=1
+    )
+
+
+def test_items_nested(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.items(True)), setup=big_nested_td, rounds=1000, iterations=1
+    )
+
+
+def test_items_nested_leaf(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.items(True, True)),
+        setup=big_nested_td,
+        rounds=1000,
+        iterations=1,
+    )
+
+
+def test_keys(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.keys()), setup=big_td, rounds=1000, iterations=1
+    )
+
+
+def test_keys_nested(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.keys(True)), setup=big_nested_td, rounds=1000, iterations=1
+    )
+
+
+def test_keys_nested_leaf(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.keys(True, True)),
+        setup=big_nested_td,
+        rounds=1000,
+        iterations=1,
+    )
+
+
+def test_values(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.values()), setup=big_td, rounds=1000, iterations=1
+    )
+
+
+def test_values_nested(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.values(True)), setup=big_nested_td, rounds=1000, iterations=1
+    )
+
+
+def test_values_nested_leaf(benchmark):
+    benchmark.pedantic(
+        lambda td: list(td.values(True, True)),
+        setup=big_nested_td,
+        rounds=1000,
+        iterations=1,
+    )
+
+
+def test_membership(benchmark):
+    benchmark.pedantic(
+        lambda td: "a" in td.keys(), setup=big_td, rounds=1000, iterations=1
+    )
+
+
+def test_membership_nested(benchmark):
+    benchmark.pedantic(
+        lambda td: ("a",) in td.keys(True),
+        setup=big_nested_td,
+        rounds=1000,
+        iterations=1,
+    )
+
+
+def test_membership_nested_leaf(benchmark):
+    benchmark.pedantic(
+        lambda td: ("a",) in td.keys(True, True),
+        setup=big_nested_td,
+        rounds=1000,
+        iterations=1,
+    )
+
+
 def test_common_ops(benchmark):
     benchmark.pedantic(main, iterations=100, rounds=100)
 
