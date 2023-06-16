561ff164c19cf89eef4db4d74c5c40e6c34bbd84
vmoens <vincentmoens@gmail.com>
2023-06-16 18:24:59 -0400

    lint

--- tensordict/nn/functional_modules.py
+++ tensordict/nn/functional_modules.py
@@ -60,8 +60,8 @@ def set_tensor(module: "torch.nn.Module", name: str, tensor: torch.Tensor) -> No
 
 
 @implement_for("torch", "2.0", None)
-def set_tensor_dict(
-    module_dict, module, name: str, tensor: torch.Tensor  # noqa: F811
+def set_tensor_dict(  # noqa: F811
+    module_dict, module, name: str, tensor: torch.Tensor
 ) -> None:
     """Simplified version of torch.nn.utils._named_member_accessor."""
     if name in module_dict["_parameters"]:
@@ -84,8 +84,8 @@ def set_tensor_dict(
 
 
 @implement_for("torch", None, "2.0")
-def set_tensor_dict(
-    module_dict, module, name: str, tensor: torch.Tensor  # noqa: F811
+def set_tensor_dict(  # noqa: F811
+    module_dict, module, name: str, tensor: torch.Tensor
 ) -> None:
     """Simplified version of torch.nn.utils._named_member_accessor."""
     if name in module_dict["_parameters"]:
