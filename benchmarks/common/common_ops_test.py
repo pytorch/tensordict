ee9be0715a2ce09501b09a17e03c3ebcf07fac28
vmoens <vincentmoens@gmail.com>
2023-06-21 09:25:26 +0100

    rename

--- benchmarks/common/common_ops_test.py
+++ benchmarks/common/common_ops_test.py
@@ -180,7 +180,7 @@ def test_values(benchmark):
     benchmark.pedantic(lambda: list(td.values()), rounds=1000, iterations=1)
 
 
-def test_values_nested_only(benchmark):
+def test_values_nested(benchmark):
     td = big_nested_td()[0][0]
     benchmark.pedantic(lambda: list(td.values(True)), rounds=1000, iterations=100)
 
