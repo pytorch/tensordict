7035376e2c124afd917b8251b454d54d94a0a9dc
Vincent Moens <vincentmoens@gmail.com>
2023-07-10 13:01:25 +0100

    Update tensordict/nn/common.py

--- tensordict/nn/common.py
+++ tensordict/nn/common.py
@@ -818,7 +818,7 @@ class TensorDictModuleBase(nn.Module):
             return
         elif parameters.ndim:
             raise NotImplementedError(
-                "reset_parameters_recursive does not support batched TensorDicts, ensure batch_size is []"
+                "reset_parameters_recursive does not support batched TensorDicts, ensure `batch_size` is empty and the parameters shape match their original shape."
             )
 
         sanitized_parameters = parameters.apply(
