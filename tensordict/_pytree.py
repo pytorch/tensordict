bfe9dcf49f4eb104b8b8d30b22f8cf364d801154
vmoens <vincentmoens@gmail.com>
2023-07-27 10:17:59 +0100

    amend

--- tensordict/_pytree.py
+++ tensordict/_pytree.py
@@ -2,7 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 
 from tensordict import (
     LazyStackedTensorDict,
