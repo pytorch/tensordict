073c4b9b56be7008211a1843a4fd84347f22055d
vmoens <vincentmoens@gmail.com>
2023-09-19 12:46:13 -0400

    amend

--- tensordict/_functional_patch.py
+++ tensordict/_functional_patch.py
@@ -5,7 +5,10 @@ import torch
 from tensordict import TensorDictBase
 from torch import Tensor
 from torch._functorch.utils import exposed_in
+from torch.nn.utils._named_member_accessor import _MISSING
+
 from torch.func import functional_call
+from functorch import dim as funcdim
 
 __all__ = []
 
@@ -37,6 +40,61 @@ def functional_call_patched(
         strict=strict,
     )
 
+def swap_tensor(
+    module: "torch.nn.Module",
+    name: str,
+    tensor: torch.Tensor,
+    allow_missing: bool = False,
+) -> torch.Tensor:
+    if not isinstance(module, torch.nn.Module):
+        raise TypeError(f"{module} is not an instance of torch.nn.Module")
+    if (
+        tensor is not _MISSING
+        and not isinstance(tensor, (torch.Tensor, funcdim.Tensor))  # <= adding funcdim.Tensor
+        and tensor is not None
+    ):
+        raise TypeError(f"{tensor} is not an instance of torch.Tensor")
+    if "." in name:
+        raise KeyError('tensor name can\'t contain "."')
+    if name == "":
+        raise KeyError('tensor name can\'t be empty string ""')
 
+    orig_tensor: torch.Tensor
+    if name in module._parameters:
+        orig_tensor = module._parameters[name]  # type: ignore[assignment]
+        if tensor is not _MISSING:
+            module._parameters[name] = tensor  # type: ignore[assignment]
+        else:
+            del module._parameters[name]
+    elif name in module._buffers:
+        orig_tensor = module._buffers[name]  # type: ignore[assignment]
+        if tensor is not _MISSING:
+            module._buffers[name] = tensor
+        else:
+            del module._buffers[name]
+    else:
+        try:
+            orig_tensor = getattr(module, name)
+        except AttributeError as ex:
+            if not allow_missing:
+                raise AttributeError(
+                    f"{module._get_name()} has no attribute `{name}`"
+                ) from ex
+            orig_tensor = _MISSING
+        if (
+            orig_tensor is not _MISSING
+            and not isinstance(orig_tensor, torch.Tensor)
+            and orig_tensor is not None
+        ):
+            raise TypeError(
+                f"attribute `{name}`: {orig_tensor} is not an instance of torch.Tensor"
+            )
+        if tensor is not _MISSING:
+            setattr(module, name, tensor)
+        elif hasattr(module, name):
+            delattr(module, name)
+    return orig_tensor
+
+torch.nn.utils._named_member_accessor.swap_tensor = swap_tensor
 torch._functorch.functional_call.functional_call = functional_call_patched
 torch.func.functional_call = functional_call_patched
