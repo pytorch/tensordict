8961a31567f5c02dbb566f67889745d07859bfa0
vmoens <vincentmoens@gmail.com>
2023-10-09 09:50:45 +0100

    amend

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -27,15 +27,15 @@ git submodule sync && git submodule update --init --recursive
 printf "Installing PyTorch with %s\n" "${CU_VERSION}"
 if [[ "$TORCH_VERSION" == "nightly" ]]; then
   if [ "${CU_VERSION:-}" == cpu ] ; then
-      pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+      python3 -m pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
   else
-      pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/$CU_VERSION
+      python3 -m pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/$CU_VERSION
   fi
 elif [[ "$TORCH_VERSION" == "stable" ]]; then
     if [ "${CU_VERSION:-}" == cpu ] ; then
-      pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+      python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
   else
-      pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/$CU_VERSION
+      python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/$CU_VERSION
   fi
 else
   printf "Failed to install pytorch"
@@ -43,10 +43,10 @@ else
 fi
 
 printf "* Installing tensordict\n"
-python setup.py develop
+python3 setup.py develop
 
 # install torchsnapshot nightly
-pip3 install git+https://github.com/pytorch/torchsnapshot --no-build-isolation
+python3 -m pip install git+https://github.com/pytorch/torchsnapshot --no-build-isolation
 
 # smoke test
 python -c "import functorch;import torchsnapshot"
