536f8a7e2199351fe95874768f56feaeaa2163b4
vmoens <vincentmoens@gmail.com>
2023-11-05 16:34:54 +0000

    amend

--- tensordict/_memory_map.py
+++ tensordict/_memory_map.py
@@ -2,6 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import os
+import tempfile
 from typing import Optional, Union
 
 import numpy as np
@@ -96,3 +98,51 @@ def ones_like(
     filename: Optional[str] = None
 ):
     return empty_like(input, memory_format=memory_format, dtype=dtype, layout=layout, device=device, pin_memory=pin_memory, requires_grad=requires_grad, filename=filename).fill_(1.0)
+
+
+class FileHandler:
+    if sys.platform == "linux":
+        _dir_candidates = ["/dev/shm"]
+    else:
+        _dir_candidates = []
+
+    def __init__(self, size, fd=-1, filename=None):
+        # borrowed from mp.heap
+        self.size = size
+        # if filename is None:
+        if fd == -1:
+            self.fd, name = tempfile.mkstemp(
+                prefix="pym-%d-" % os.getpid(), dir=self._choose_dir(size)
+            )
+            # self.filename = name
+            os.unlink(name)
+            util.Finalize(self, os.close, (self.fd,))
+            os.ftruncate(self.fd, size)
+        else:
+            self.fd = fd
+        # else:
+        #     self.filename = filename
+        self.buffer = mmap.mmap(self.fd, self.size)
+
+    def _choose_dir(self, size):
+        # Choose a non-storage backed directory if possible,
+        # to improve performance
+        for d in self._dir_candidates:
+            st = os.statvfs(d)
+            if st.f_bavail * st.f_frsize >= size:  # enough free space?
+                return d
+        tmpdir = util.get_temp_dir()
+        return tmpdir
+
+
+def reduce_handler(handler):
+    if handler.fd == -1:
+        raise ValueError(
+            "Handler is unpicklable because " "forking was enabled when it was created"
+        )
+    return rebuild_handler, (handler.size, reduction.DupFd(handler.fd))
+
+
+def rebuild_handler(size, dupfd):
+    detached = dupfd.detach()
+    return FileHandler(size, detached)
