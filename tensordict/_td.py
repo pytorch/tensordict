86c239ff93532a56ee848f0de1248624c3d1ffa4
Vincent Moens <vmoens@meta.com>
2023-12-04 11:44:05 +0000

    [Performance] Make copy_ a no-op if tensors are identical (#588)

--- tensordict/_td.py
+++ tensordict/_td.py
@@ -1287,7 +1287,8 @@ class TensorDict(TensorDictBase):
                 if best_attempt and _is_tensor_collection(dest.__class__):
                     dest.update(value, inplace=True)
                 else:
-                    dest.copy_(value)
+                    if dest is not value:
+                        dest.copy_(value, non_blocking=True)
             except KeyError as err:
                 raise err
             except Exception as err:
