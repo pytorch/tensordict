6e0a3df75ca92561eb65b3a638931846677be881
vmoens <vincentmoens@gmail.com>
2023-12-16 13:13:46 +0000

    amend

--- test/test_distributed.py
+++ test/test_distributed.py
@@ -68,7 +68,7 @@ class TestFSDP:
             init_method="tcp://localhost:10017",
         )
         module = cls.make_module()
-        print('module created')
+        print('module created on 1')
         q.put("done")
 
     @classmethod
@@ -80,8 +80,9 @@ class TestFSDP:
             init_method="tcp://localhost:10017",
         )
         module = cls.make_module()
-        print('module created')
-        td = TensorDict.from_module(module, use_state_dict=True)
+        print('module created on 0')
+        td = TensorDict(module.state_dict(), []).unflatten_keys(".")
+        # td = TensorDict.from_module(module, use_state_dict=True)
         print('td created')
         td.memmap(path)
         print('memmaped!')
