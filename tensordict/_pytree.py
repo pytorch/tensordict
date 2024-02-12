d5e8e4c1dddf9a37aa4ff2df32742c0083cda551
vmoens <vincentmoens@gmail.com>
2024-02-12 21:10:24 +0000

    init

--- tensordict/_pytree.py
+++ tensordict/_pytree.py
@@ -9,10 +9,11 @@ from tensordict import (
     PersistentTensorDict,
     SubTensorDict,
     TensorDict,
+    TensorDictBase,
 )
 
 try:
-    from torch.utils._pytree import Context, register_pytree_node
+    from torch.utils._pytree import Context, MappingKey, register_pytree_node
 except ImportError:
     from torch.utils._pytree import (
         _register_pytree_node as register_pytree_node,
@@ -80,8 +81,10 @@ def _str_to_tensordictdict(str_spec: str) -> Tuple[List[str], str]:
 
 
 def _tensordict_flatten(d: TensorDict) -> Tuple[List[Any], Context]:
-    return list(d.values()), {
-        "keys": list(d.keys()),
+    keys, values = zip(*d.items())
+    keys = list(keys)
+    return list(values), {
+        "keys": keys,
         "batch_size": d.batch_size,
         "names": d.names,
         "device": d.device,
@@ -89,6 +92,7 @@ def _tensordict_flatten(d: TensorDict) -> Tuple[List[Any], Context]:
 
 
 def _tensordictdict_unflatten(values: List[Any], context: Context) -> Dict[Any, Any]:
+    # print("context", context, "values", values)
     device = context["device"]
     if device is not None:
         device = (
@@ -104,9 +108,24 @@ def _tensordictdict_unflatten(values: List[Any], context: Context) -> Dict[Any, 
     )
 
 
+def _td_flatten_with_keys(
+    d: TensorDictBase,
+):
+    keys, values = zip(*d.items())
+    values = list(values)
+    keys = list(keys)
+    return [(MappingKey(k), v) for k, v in zip(keys, values)], {
+        "keys": keys,
+        "batch_size": d.batch_size,
+        "names": d.names,
+        "device": d.device,
+    }
+
+
 for cls in PYTREE_REGISTERED_TDS:
     register_pytree_node(
         cls,
         _tensordict_flatten,
         _tensordictdict_unflatten,
+        flatten_with_keys_fn=_td_flatten_with_keys,
     )
