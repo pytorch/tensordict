efdd277c479c789dae8a70a2d03ecb1b74b5b05e
Vincent Moens <vmoens@meta.com>
2024-05-24 16:45:41 -0400

    amend

--- tensordict/tensorclass.py
+++ tensordict/tensorclass.py
@@ -210,6 +210,9 @@ def _tensorclass(cls: T) -> T:
             )
 
     cls.fields = classmethod(lambda cls: dataclasses.fields(cls))
+    for field in cls.fields():
+        if hasattr(cls, field.name):
+            delattr(cls, field.name)
 
     _get_type_hints(cls)
     cls.__init__ = _init_wrapper(cls.__init__)
@@ -219,9 +222,10 @@ def _tensorclass(cls: T) -> T:
         cls.__torch_function__ = classmethod(__torch_function__)
     cls.__getstate__ = _getstate
     cls.__setstate__ = _setstate
-    cls.__getattribute__ = _getattribute_wrapper(cls.__getattribute__)
-    cls.__setattr__ = _setattr_wrapper(cls.__setattr__, expected_keys)
+    # cls.__getattribute__ = object.__getattribute__
     cls.__getattr__ = _getattr
+    cls.__setattr__ = _setattr_wrapper(cls.__setattr__, expected_keys)
+    # cls.__getattr__ = _getattr
     cls.__getitem__ = _getitem
     cls.__getitems__ = _getitem
     cls.__setitem__ = _setitem
@@ -669,41 +673,32 @@ def _setstate(self, state: dict[str, Any]) -> None:  # noqa: D417
     self._non_tensordict = state.get("non_tensordict", None)
 
 
-def _getattribute_wrapper(getattribute: Callable) -> Callable:
-    """Retrieve the value of an object's attribute or raise AttributeError.
-
-    Args:
-        item (str) : name of the attribute to retrieve
-
-    Returns:
-        value of the attribute
-
-    """
-
-    @functools.wraps(getattribute)
-    def wrapper(self, item: str) -> Any:
-        if not item.startswith("__"):
-            if (
-                "_tensordict" in self.__dict__
-                and item in self.__dict__["_tensordict"].keys()
-            ):
-                out = self._tensordict.get(item)
+def _getattr(self, item: str) -> Any:
+    # if not item.startswith("__"):
+    __dict__ = self.__dict__
+    _tensordict = __dict__.get("_tensordict")
+    if _tensordict is not None:
+        out = _tensordict._get_str(item, default=None)
+        if out is not None:
+            return out
+        out = getattr(_tensordict, item, NO_DEFAULT)
+        if out is not NO_DEFAULT:
+            if not callable(out):
                 return out
-            elif (
-                "_non_tensordict" in self.__dict__
-                and item in self.__dict__["_non_tensordict"]
-            ):
-                out = self._non_tensordict[item]
-                if (
-                    isinstance(self, NonTensorData)
-                    and item == "data"
-                    and (self._is_shared or self._is_memmap)
-                ):
-                    return _from_shared_nontensor(out)
-                return out
-        return getattribute(self, item)
-
-    return wrapper
+            return _wrap_method(self, item, out)
+    _non_tensordict = __dict__.get("_non_tensordict")
+    if _non_tensordict is not None:
+        out = _non_tensordict.get(item, NO_DEFAULT)
+        if out is NO_DEFAULT:
+            raise AttributeError
+        if (
+            isinstance(self, NonTensorData)
+            and item == "data"
+            and (self._is_shared or self._is_memmap)
+        ):
+            return _from_shared_nontensor(out)
+        return out
+    raise AttributeError
 
 
 SET_ATTRIBUTES = ("batch_size", "device", "_locked_tensordicts", "names")
@@ -871,22 +866,6 @@ def _wrap_classmethod(td_cls, cls, func):
 
     return wrapped_func
 
-
-def _getattr(self, attr: str) -> Any:
-    """Retrieve the value of an object's attribute, or a method output if attr is callable.
-
-    Args:
-        attr: name of the attribute to retrieve or function to compute
-
-    Returns:
-        value of the attribute, or a method output applied on the instance
-
-    """
-    res = getattr(self._tensordict, attr)
-    if not callable(res):
-        return res
-    func = res
-    return _wrap_method(self, attr, func)
 
 
 def _getitem(self, item: NestedKey) -> Any:
--- test/test_compile.py
+++ test/test_compile.py
@@ -7,8 +7,9 @@ import argparse
 import pytest
 
 import torch
-from tensordict import assert_close, TensorDict
+from tensordict import assert_close, TensorDict, tensorclass
 from tensordict.nn import TensorDictModule as Mod, TensorDictSequential as Seq
+from typing import Any
 
 
 class TestTD:
@@ -110,9 +111,112 @@ class TestTD:
         else:
             assert clone_c(data)["a", "b"] is data["a", "b"]
 
+@tensorclass
+class MyClass:
+    a: "MyClass"
+    b: Any=None
+    c: Any=None
 
 class TestTC:
-    pass
+
+    def test_tensor_output(self):
+        def add_one(td):
+            return td.a.b + 1
+
+        add_one_c = torch.compile(add_one, fullgraph=True)
+        data = MyClass(MyClass(a=None, b=0))
+        assert add_one(data) == 1
+        assert add_one_c(data) == 1
+        assert add_one_c(data + 1) == 2
+
+    def test_td_output(self):
+        def add_one(td):
+            td.a.c = td.a.b + 1
+            return td
+
+        add_one_c = torch.compile(add_one, fullgraph=True)
+        data = MyClass.from_tensordict(TensorDict({"a": {"b": 0}}))
+        assert add_one(data.clone()).a.c == 1
+        assert add_one_c(data.clone()).a.c == 1
+        assert add_one_c(data) is data
+
+    @pytest.mark.parametrize("index_type", ["slice", "tensor", "int"])
+    def test_td_index(self, index_type):
+        if index_type == "slice":
+
+            def add_one(td):
+                return td[:2] + 1
+
+        elif index_type == "tensor":
+
+            def add_one(td):
+                return td[torch.tensor([0, 1])] + 1
+
+        elif index_type == "int":
+
+            def add_one(td):
+                return td[0] + 1
+
+        add_one_c = torch.compile(add_one, fullgraph=True)
+        data = TensorDict({"a": {"b": torch.arange(3)}}, [3])
+        if index_type == "int":
+            assert (add_one(data)["a", "b"] == 1).all()
+            assert (add_one_c(data)["a", "b"] == 1).all()
+            assert add_one_c(data).shape == torch.Size([])
+        else:
+            assert (add_one(data)["a", "b"] == torch.arange(1, 3)).all()
+            assert (add_one_c(data)["a", "b"] == torch.arange(1, 3)).all()
+            assert add_one_c(data).shape == torch.Size([2])
+
+    def test_stack(self):
+        def stack_tds(td0, td1):
+            return TensorDict.stack([td0, td1])
+            # return torch.stack([td0, td1])
+
+        stack_tds_c = torch.compile(stack_tds, fullgraph=True)
+        data0 = TensorDict({"a": {"b": torch.arange(3)}}, [3])
+        data1 = TensorDict({"a": {"b": torch.arange(3)}}, [3])
+        assert (stack_tds(data0, data1) == stack_tds_c(data0, data1)).all()
+
+    def test_cat(self):
+        def cat_tds(td0, td1):
+            return TensorDict.cat([td0, td1])
+
+        cat_tds_c = torch.compile(cat_tds, fullgraph=True)
+        data0 = TensorDict({"a": {"b": torch.arange(3)}}, [3])
+        data1 = TensorDict({"a": {"b": torch.arange(3)}}, [3])
+        assert (cat_tds(data0, data1) == cat_tds_c(data0, data1)).all()
+
+    def test_reshape(self):
+        def reshape(td):
+            return td.reshape(2, 2)
+
+        reshape_c = torch.compile(reshape, fullgraph=True)
+        data = TensorDict({"a": {"b": torch.arange(4)}}, [4])
+        assert (reshape(data) == reshape_c(data)).all()
+
+    def test_unbind(self):
+        def unbind(td):
+            return td.unbind(0)
+
+        unbind_c = torch.compile(unbind, fullgraph=True)
+        data = TensorDict({"a": {"b": torch.arange(4)}}, [4])
+        assert (unbind(data)[-1] == unbind_c(data)[-1]).all()
+
+    @pytest.mark.parametrize("recurse", [True, False])
+    def test_clone(self, recurse):
+        def clone(td: TensorDict):
+            return td.clone(recurse=recurse)
+
+        clone_c = torch.compile(clone, fullgraph=True)
+        data = TensorDict({"a": {"b": 0, "c": 1}})
+        assert_close(clone_c(data), clone(data))
+        assert clone_c(data) is not data
+        if recurse:
+            assert clone_c(data)["a", "b"] is not data["a", "b"]
+        else:
+            assert clone_c(data)["a", "b"] is data["a", "b"]
+
 
 
 class TestNN:
