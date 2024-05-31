19f95458c011412b1496abd0a0a051736637fb22
Vincent Moens <vmoens@meta.com>
2024-05-31 18:02:59 +0100

    amend

--- tensordict/_td.py
+++ tensordict/_td.py
@@ -264,7 +264,8 @@ class TensorDict(TensorDictBase):
                 )
             self._batch_size = self._parse_batch_size(source, batch_size)
             # TODO: this breaks when stacking tensorclasses with dynamo
-            # self.names = names
+            if not torch.compiler.is_dynamo_compiling():
+                self.names = names
 
             for key, value in source.items():
                 self.set(key, value, non_blocking=sub_non_blocking)
