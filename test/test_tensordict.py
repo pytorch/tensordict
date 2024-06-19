af032a7ca9e2082e53cc98ffc534058b03c2673e
Vincent Moens <vmoens@meta.com>
2024-06-19 12:56:16 +0100

    amend

--- test/test_tensordict.py
+++ test/test_tensordict.py
@@ -5637,7 +5637,7 @@ class TestTensorDicts(TestTensorDictsBase):
             with pytest.raises(RuntimeError, match="a leaf Variable"):
                 sub_td.update(td0)
             return
-        print('updating', sub_td, 'with', td0)
+        print("updating", sub_td, "with", td0)
         sub_td.update(td0)
         assert (sub_td == 2).all()
         assert (td[index] == 2).all()
@@ -9075,9 +9075,7 @@ class TestNonTensorData:
 
         # with pytest.raises(RuntimeError)
         val1 = MyClass(string="another string!")
-        with pytest.raises(
-            ValueError, match="Failed to update 'val' in tensordict"
-        ):
+        with pytest.raises(ValueError, match="Failed to update 'val' in tensordict"):
             td.update(
                 TensorDict({"val": NonTensorData(data=val1, batch_size=[])}, []),
                 inplace=True,
