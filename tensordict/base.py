8bb252eacf4945444c95bc7b072ce52ee041c03c
Vincent Moens <vmoens@meta.com>
2024-06-21 09:19:33 +0100

    amend

--- tensordict/base.py
+++ tensordict/base.py
@@ -2835,9 +2835,6 @@ class TensorDictBase(MutableMapping):
         if filename is not None:
             if use_buffer:
                 with open(filename, "w+b") as f:
-                    # storage._handler.buffer.write_byte(f.read(1))
-                    # storage._handler.buffer.flush()
-                    # os.sendfile(f.fileno(), storage._handler.fd, 0, filesize)
                     f.write(storage._handler.buffer)
             with open(Path(filename).with_suffix(".json"), "wb") as f:
                 metadata_dict["size"] = filesize
