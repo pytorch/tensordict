edc315b605682f23d5a7ed27f73b0165303c243e
Vincent Moens <vmoens@meta.com>
2024-07-08 16:23:18 +0100

    lint

--- tensordict/_tensordict.py
+++ tensordict/_tensordict.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import warnings
-from tensordict._C import *
+from tensordict._C import *  # noqa
 
 warnings.warn(
     "tensordict._tensordict will soon be removed in favour of tensordict._C.",
