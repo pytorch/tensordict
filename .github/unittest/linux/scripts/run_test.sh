2b780cc071523c8f7a5ceee7fe76a1fa84ea10fa
Vincent Moens <vmoens@meta.com>
2024-07-10 16:39:50 +0100

    amend

--- .github/unittest/linux/scripts/run_test.sh
+++ .github/unittest/linux/scripts/run_test.sh
@@ -19,6 +19,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
 export MKL_THREADING_LAYER=GNU
 
 coverage run -m pytest test/smoke_test.py -v --durations 20
-coverage run -m pytest --instafail -v --durations 20
+coverage run -m pytest --instafail -v --durations 20 --timeout=30
 coverage run -m pytest ./benchmarks --instafail -v --durations 20
 coverage xml -i
