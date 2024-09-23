e1188b10cf38a6155f5012e6322af9bd96c1a0ac
Vincent Moens <vmoens@meta.com>
2024-09-23 12:34:24 +0100

    Update
    [ghstack-poisoned]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ else:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # -- General configuration ---------------------------------------------------
 
