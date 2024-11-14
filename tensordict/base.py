c11024e0db864bd8487c68e3d0cf5e100fc07831
Vincent Moens <vmoens@meta.com>
2024-11-14 13:58:25 +0000

    [BugFix] Fix none ref in during reduction (#1090)

--- tensordict/base.py
+++ tensordict/base.py
@@ -10965,7 +10965,7 @@ class TensorDictBase(MutableMapping):
                     tgt = mb_unwrap_functional_tensor(new_thing)
                     src = mb_unwrap_functional_tensor(ragged_source)
                     tgt.nested_int_memo = src.nested_int_memo
-                else:
+                elif new_thing is not None:
                     _tensor_symint_registry[new_thing] = _tensor_symint_registry[
                         ragged_source
                     ]
