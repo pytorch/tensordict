c4ef995b65f62b3b6dfc304824a94e747106c79f
Vincent Moens <vmoens@meta.com>
2024-12-04 11:32:54 +0000

    Update
    [ghstack-poisoned]

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -2,8 +2,16 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import argparse
+
+import pytest
 
 
 def test_imports():
     from tensordict import TensorDict  # noqa: F401
     from tensordict.nn import TensorDictModule  # noqa: F401
+
+
+if __name__ == "__main__":
+    args, unknown = argparse.ArgumentParser().parse_known_args()
+    pytest.main([__file__, "--capture", "no", "--exitfirst"] + unknown)
