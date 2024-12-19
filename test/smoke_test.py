6d6e553950dce8e669fba4b9f9fe12d5763b8458
Vincent Moens <vmoens@meta.com>
2024-12-19 08:44:35 +0000

    Update (base update)
    [ghstack-poisoned]

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -2,10 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import argparse
-import sys
-
-import pytest
 
 
 def test_imports():
@@ -22,5 +18,4 @@ def test_imports():
 
 
 if __name__ == "__main__":
-    args, unknown = argparse.ArgumentParser().parse_known_args()
-    pytest.main([__file__, "--capture", "no", "--exitfirst"] + unknown)
+    test_imports()
