d25bd542accd0aaa7ec9916b04e45d8abfa7fe14
Vincent Moens <vmoens@meta.com>
2025-02-24 14:41:04 +0000

    [BugFix] _PASSTHROUGH_MEMO for passthrough tensorclass
    ghstack-source-id: 0bfbfc9f6700f1165fcfd6b38f65fa4fd806be80
    Pull Request resolved: https://github.com/pytorch/tensordict/pull/1231

--- tensordict/utils.py
+++ tensordict/utils.py
@@ -2582,17 +2582,20 @@ def _is_non_tensor(cls: type):
     return out
 
 
+_PASSTHROUGH_MEMO = {}
+
+
 def _pass_through_cls(cls: type):
     out = None
     is_dynamo = is_compiling()
     if not is_dynamo:
-        out = _NON_TENSOR_MEMO.get(cls)
+        out = _PASSTHROUGH_MEMO.get(cls)
     if out is None:
         out = bool(getattr(cls, "_is_non_tensor", False)) or getattr(
             cls, "_pass_through", False
         )
         if not is_dynamo:
-            _NON_TENSOR_MEMO[cls] = out
+            _PASSTHROUGH_MEMO[cls] = out
     return out
 
 
