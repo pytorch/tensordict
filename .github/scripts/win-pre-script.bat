5c9cd2c66cc00087ac32ac1236fc08df86f0d4d8
Vincent Moens <vmoens@meta.com>
2025-03-10 17:59:33 +0000

    Update
    [ghstack-poisoned]

--- .github/scripts/win-pre-script.bat
+++ .github/scripts/win-pre-script.bat
@@ -6,7 +6,7 @@ if "%CONDA_RUN%"=="" (
 )
 
 :: Run the pip install command
-%CONDA_RUN% pip install cmake pybind11
+%CONDA_RUN% python -m pip install cmake pybind11 -U
 
 :: Check if the installation was successful
 if errorlevel 1 (
