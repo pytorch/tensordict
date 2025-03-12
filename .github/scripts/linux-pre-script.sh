1f63880973bb906a730d7eb6ec283d7a3246fd97
Vincent Moens <vmoens@meta.com>
2025-03-12 17:18:43 +0000

    Update
    [ghstack-poisoned]

--- .github/scripts/linux-pre-script.sh
+++ .github/scripts/linux-pre-script.sh
@@ -4,8 +4,12 @@ if [ "$(uname)" != "Darwin" ]; then
   yum update gcc
   yum update libstdc++
 else
+  brew update
+  brew upgrade gcc
+
   # For OSX
   export CXXFLAGS="-march=armv8-a+fp16+sha3"
+  export CMAKE_OSX_ARCHITECTURES=arm64
 fi
 
 ${CONDA_RUN} conda install -c conda-forge pybind11 -y
--- .github/scripts/version_script.sh
+++ .github/scripts/version_script.sh
@@ -5,6 +5,7 @@ export TENSORDICT_BUILD_VERSION=0.7.0
 if [ "$(uname)" == "Darwin" ]; then
   # For OSX
   export CXXFLAGS="-march=armv8-a+fp16+sha3"
+  export CMAKE_OSX_ARCHITECTURES=arm64
 fi
 
 ${CONDA_RUN} conda install -c conda-forge pybind11 -y
