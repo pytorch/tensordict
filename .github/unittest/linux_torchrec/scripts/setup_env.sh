6533c45914a9e90b6a4618e344cd4ce9369cefbf
Vincent Moens <vmoens@meta.com>
2025-03-12 17:18:43 +0000

    [Setup] Making tensordict pytorch-agnostic
    ghstack-source-id: 181b17481d8cce164afd46db577ec19483878df4
    Pull Request resolved: https://github.com/pytorch/tensordict/pull/1256

--- .github/unittest/linux_torchrec/scripts/setup_env.sh
+++ .github/unittest/linux_torchrec/scripts/setup_env.sh
@@ -47,3 +47,6 @@ cat "${this_dir}/environment.yml"
 pip install pip --upgrade
 
 conda env update --file "${this_dir}/environment.yml" --prune
+
+conda install anaconda::cmake -y
+conda install -c conda-forge pybind11 -y
