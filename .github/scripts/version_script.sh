5ca6d8c14b413d04dbe6c4e3b523f6fd67c8d6e2
Vincent Moens <vmoens@meta.com>
2025-04-04 09:35:04 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/version_script.sh
+++ .github/scripts/version_script.sh
@@ -4,7 +4,12 @@ export TENSORDICT_BUILD_VERSION=0.8.0
 
 if [ "$(uname)" == "Darwin" ]; then
   # For OSX
-  export CXXFLAGS="-march=armv8-a+fp16+sha3"
+  echo $(gcc --version)
+  echo $(clang --version)
+  brew update
+  brew upgrade gcc
+  brew upgrade clang
+#  export CXXFLAGS="-march=armv8-a+fp16+sha3"
   export CMAKE_OSX_ARCHITECTURES=arm64
 fi
 
