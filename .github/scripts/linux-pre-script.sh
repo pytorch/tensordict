c6996c9e8f9cf920b96b70ce866dcd551eddc6e4
Vincent Moens <vmoens@meta.com>
2025-04-04 09:24:46 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/linux-pre-script.sh
+++ .github/scripts/linux-pre-script.sh
@@ -4,11 +4,14 @@ if [ "$(uname)" != "Darwin" ]; then
   yum update gcc
   yum update libstdc++
 else
+  echo $(gcc --version)
+  echo $(clang --version)
   brew update
   brew upgrade gcc
+  brew upgrade clang
 
   # For OSX
-  export CXXFLAGS="-march=armv8-a+fp16+sha3"
+#  export CXXFLAGS="-march=armv8-a+fp16+sha3"
   export CMAKE_OSX_ARCHITECTURES=arm64
 fi
 
