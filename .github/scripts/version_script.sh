018c9ec54f461422064514e579fbb79c18e61fc4
Vincent Moens <vmoens@meta.com>
2025-04-14 10:27:53 +0100

    [Setup] Making tensordict pytorch-agnostic
    ghstack-source-id: fc94537f282dbe7f9f7833c7741629e034548745
    Pull Request resolved: https://github.com/pytorch/tensordict/pull/1256

--- .github/scripts/version_script.sh
+++ .github/scripts/version_script.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 export TENSORDICT_BUILD_VERSION=0.8.0
+
+if [ "$(uname)" == "Darwin" ]; then
+  # For OSX
+  echo $(gcc --version)
+  echo $(clang --version)
+  brew update
+  brew install gcc
+  brew install clang-build-analyzer
+  brew install --cask clay
+  brew install llvm
+#  brew upgrade gcc
+#  brew upgrade clang
+#  export CXXFLAGS="-march=armv8-a+fp16+sha3"
+  export CMAKE_OSX_ARCHITECTURES=arm64
+fi
+
+${CONDA_RUN} conda install -c conda-forge pybind11 -y
