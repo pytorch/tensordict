2ee4255d741352a804b1fc7d1b6c0ccd320ea28a
Vincent Moens <vmoens@meta.com>
2025-04-23 12:49:21 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/version_script.sh
+++ .github/scripts/version_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export TENSORDICT_BUILD_VERSION=0.8.0
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh
