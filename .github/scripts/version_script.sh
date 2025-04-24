b9fdac816e9464cf8df49c7474e8f7af7deaba48
Vincent Moens <vmoens@meta.com>
2025-04-24 14:11:20 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/version_script.sh
+++ .github/scripts/version_script.sh
@@ -9,19 +9,19 @@ ${CONDA_RUN} conda install conda-forge::rust -y
 # for orjson
 export UNSAFE_PYO3_BUILD_FREE_THREADED=1
 
-#if [ "$(uname)" == "Darwin" ]; then
-#  # For OSX
-#  echo $(gcc --version)
-#  echo $(clang --version)
-#  brew update
-#  brew install gcc
-#  brew install clang-build-analyzer
-#  brew install --cask clay
-#  brew install llvm
-##  brew upgrade gcc
-##  brew upgrade clang
-##  export CXXFLAGS="-march=armv8-a+fp16+sha3"
-#  export CMAKE_OSX_ARCHITECTURES=arm64
-#fi
+if [ "$(uname)" == "Darwin" ]; then
+  # For OSX
+  echo $(gcc --version)
+  echo $(clang --version)
+  brew update
+  brew install gcc
+  brew install clang-build-analyzer
+  brew install --cask clay
+  brew install llvm
+#  brew upgrade gcc
+#  brew upgrade clang
+#  export CXXFLAGS="-march=armv8-a+fp16+sha3"
+  export CMAKE_OSX_ARCHITECTURES=arm64
+fi
 
 ${CONDA_RUN} conda install -c conda-forge pybind11 -y
