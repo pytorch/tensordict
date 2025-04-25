6d985bb2cddfc2d8b8cee971463a4a4a4f712134
Vincent Moens <vmoens@meta.com>
2025-04-25 13:44:07 +0100

    Update
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ class CMakeBuild(build_ext):
             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
         )
         subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+            ["cmake", "--build", ".", "--verbose"] + build_args, cwd=self.build_temp
         )
 
 
