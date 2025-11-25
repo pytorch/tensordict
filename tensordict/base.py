3db9a7c56fc8eb3a56a6f04f7bbd66e3c3934dce
Kevin Wang <kevmo314@gmail.com>
2025-11-25 15:56:27 +0000

    Fix race condition

--- tensordict/base.py
+++ tensordict/base.py
@@ -653,7 +653,6 @@ class TensorDictBase(MutableMapping, TensorCollection):
             "_last_op",
             "_cache",
             "__lock_parents_weakrefs",
-            "_validate_value_cached",
         ):
             result.pop(key, None)
         return result
@@ -3491,8 +3490,6 @@ class TensorDictBase(MutableMapping, TensorCollection):
         return self._dtype()
 
     def _batch_size_setter(self, new_batch_size: torch.Size) -> None:
-        if self._validate_value_cached is not None:
-            delattr(self, "_validate_value_cached")
         if new_batch_size == self.batch_size:
             return
         if self._lazy:
@@ -5240,8 +5237,6 @@ class TensorDictBase(MutableMapping, TensorCollection):
 
         """
         self._device = None
-        if self._validate_value_cached is not None:
-            delattr(self, "_validate_value_cached")
         for value in self.values():
             if _is_tensor_collection(type(value)):
                 value.clear_device_()
@@ -5249,8 +5244,6 @@ class TensorDictBase(MutableMapping, TensorCollection):
 
     def _set_device(self, device: torch.device) -> Self:
         self._device = device
-        if self._validate_value_cached is not None:
-            delattr(self, "_validate_value_cached")
         for value in self.values():
             if _is_tensor_collection(type(value)):
                 value._set_device(device=device)
@@ -12399,26 +12392,21 @@ class TensorDictBase(MutableMapping, TensorCollection):
             raise KeyError(_GENERIC_NESTED_ERR.format(key))
         return key
 
-    _validate_value_cached: str | None = None
-
     @property
     def _validate_value(self):
         if is_compiling():
             return self._validate_value_generic
-        _validate_value_cached = self._validate_value_cached
-        if _validate_value_cached is None:
-            if self.device:
-                if self.batch_size:
-                    _validate_value_cached = "_validate_value_generic"
-                else:
-                    _validate_value_cached = "_validate_value_batchfree"
+        if self.device:
+            if self.batch_size:
+                method_name = "_validate_value_generic"
             else:
-                if self.batch_size:
-                    _validate_value_cached = "_validate_value_devicefree"
-                else:
-                    _validate_value_cached = "_validate_value_batchfree_devicefree"
-            self._validate_value_cached = _validate_value_cached
-        return getattr(self, _validate_value_cached)
+                method_name = "_validate_value_batchfree"
+        else:
+            if self.batch_size:
+                method_name = "_validate_value_devicefree"
+            else:
+                method_name = "_validate_value_batchfree_devicefree"
+        return getattr(self, method_name)
 
     def _validate_value_generic(
         self,
